1943eeae3f08465f9d9eb7bec7e3e98084c8fd4f
Miko Cuevas <julesmiko.cuevas.iics@ust.edu.ph>
2022-05-05 14:03:34 +0800

    testing heroku mail server

--- includes/contact.inc.php
+++ includes/contact.inc.php
@@ -21,8 +21,9 @@ if (isset($_POST["contact-us-submit"])){
 
 	try {
 		//Server settings
-		$mail->isSMTP();                                            //Send using SMTP
-		$mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+		$mail->isSMTP(); 
+		$mail->Host       = 'smtp.sendgrid.net';    //heroku mail server                                        //Send using SMTP
+		// $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
 		$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
 		$mail->Username   = 'mchacks996@gmail.com';                     //SMTP username
 		$mail->Password   = '!@#$%^&*()asdfghjkl';                               //SMTP 
